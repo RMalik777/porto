2a73e3725c6a9f2afc5579a09ecb7541e8be60c1
Rafli Malik <Rafli.Malik@gmail.com>
2024-01-15 23:27:59 +0700

    Upgrade Speed Insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/Footer";
