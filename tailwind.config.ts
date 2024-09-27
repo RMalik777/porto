3c1c50d0d1b86efdc8bb2b795b340367af965765
Rafli Malik <Rafli.Malik@gmail.com>
2024-09-27 15:48:59 +0700

    chore: add shadcn/ui prep

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,23 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
+	darkMode: ["class"],
 	content: [
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}'
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-	theme: {},
-	plugins: []
+	theme: {
+		extend: {
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			colors: {},
+		},
+	},
+	plugins: [tailwindcssAnimate],
 };
 export default config;
