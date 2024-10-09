32cec3fa70aa99c42c9bc418f3c87676277de481
Rafli Malik <Rafli.Malik@gmail.com>
2024-10-09 09:13:54 +0700

    feat: custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
+export const metadata = {
+	title: {
+		absolute: "404 Not Found | Rafli Malik",
+	},
+	description: "Page not found.",
+};
+
+export default function notFound() {
+	return (
+		<main className="flex h-dvh min-h-dvh flex-col items-center justify-center gap-8 tracking-tight">
+			<div className="placeholder h-10 w-full bg-red-100"></div>
+			<section className="flex flex-col items-center justify-center gap-4 text-center leading-none">
+				<h1 className="bg-linear-to-r from-purple-500 to-pink-500 bg-clip-text text-3xl font-semibold tracking-tighter text-transparent sm:text-4xl md:text-5xl">
+					404
+				</h1>
+				<p>It&apos;s looks like you are lost</p>
+				<Button asChild variant="outline" size="sm">
+					<Link href="/">Home</Link>
+				</Button>
+			</section>
+		</main>
+	);
+}
