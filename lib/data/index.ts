be0427737dc4f831441f1d2af85e51fd6e46d23d
Rafli Malik <Rafli.Malik@gmail.com>
2024-10-10 17:22:58 +0700

    chore: add project data and skill data

---
+++ lib/data/index.ts
@@ -0,0 +1,4 @@
+import { skillsList } from "./skill";
+import { projectsList } from "./project";
+
+export { skillsList, projectsList };
---
+++ lib/data/skill.ts
@@ -0,0 +1,158 @@
+import { siHtml5, siCss3, siJavascript } from "simple-icons";
+
+interface Skill {
+	category: string;
+	children: {
+		name: string;
+		link: string;
+		logo?: string;
+		logoAlt?: string;
+		color?: string;
+	}[];
+}
+
+export const skillsList: Skill[] = [
+	{
+		category: "Front-End",
+		children: [
+			{
+				name: "HTML",
+				link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+				color: siHtml5.hex,
+				logo: "/Logo/[HTML5]+CC by 3.0.svg",
+			},
+			{
+				name: "CSS",
+				link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+				color: siCss3.hex,
+				logo: "/Logo/[CSS]+CC by 3.0.svg",
+			},
+			{
+				name: "JavaScript",
+				link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+				color: siJavascript.hex,
+				logo: "/Logo/[JS]+MIT.svg",
+			},
+		],
+	},
+	{
+		category: "CSS Frameworks",
+		children: [
+			{
+				name: "Tailwind CSS",
+				link: "https://tailwindcss.com/",
+				logo: "/Logo/[TAILWIND CSS]+Proprietary.svg",
+			},
+			{
+				name: "Bootstrap",
+				link: "https://getbootstrap.com/",
+				logo: "/Logo/[BOOTSTRAP]_light+CC by 3.0.svg",
+				logoAlt: "/Logo/[BOOTSTRAP]_dark+CC by 3.0.svg",
+			},
+		],
+	},
+	{
+		category: "Front-End Frameworks",
+		children: [
+			{
+				name: "React",
+				link: "https://reactjs.org/",
+				logo: "/Logo/[REACT]_light+CC by 4.0.svg",
+				logoAlt: "/Logo/[REACT]_dark+CC by 4.0.svg",
+			},
+			{
+				name: "Next.js",
+				link: "https://nextjs.org/",
+				logo: "/Logo/[NEXT JS]_light+Proprietary.svg",
+				logoAlt: "/Logo/[NEXT JS]_dark+Proprietary.svg",
+			},
+			{
+				name: "Remix",
+				link: "https://remix.run/",
+				logo: "/Logo/[REMIX]_light+Proprietary.svg",
+				logoAlt: "/Logo/[REMIX]_dark+Proprietary.svg",
+			},
+			{
+				name: "Svelte",
+				link: "https://svelte.dev/",
+				logo: "/Logo/[SVELTE]+Proprietary.svg",
+			},
+			{
+				name: "SvelteKit",
+				link: "https://kit.svelte.dev/",
+				logo: "/Logo/[SVELTE]+Proprietary.svg",
+			},
+			{
+				name: "Astro",
+				link: "https://astro.build/",
+				logo: "/Logo/[ASTRO]_light+MIT.svg",
+				logoAlt: "/Logo/[ASTRO]_dark+MIT.svg",
+			},
+			{
+				name: "Vue",
+				link: "https://vuejs.org/",
+				logo: "/Logo/[VUE]_light+CC by-nc-sa 4.0.svg",
+				logoAlt: "/Logo/[VUE]_dark+CC by-nc-sa 4.0.svg",
+			},
+			{
+				name: "Nuxt",
+				link: "https://nuxtjs.org/",
+				logo: "/Logo/[NUXT]+MIT.svg",
+			},
+		],
+	},
+	{
+		category: "Animation",
+		children: [
+			{
+				name: "Framer Motion",
+				link: "https://www.framer.com/motion/",
+				logo: "/Logo/[FRAMER]_light+Proprietary.svg",
+				logoAlt: "/Logo/[FRAMER]_dark+Proprietary.svg",
+			},
+			{
+				name: "GSAP",
+				link: "https://gsap.com/",
+			},
+		],
+	},
+	{
+		category: "Back-End",
+		children: [
+			{
+				name: "Laravel",
+				link: "https://laravel.com/",
+			},
+			{
+				name: "Tauri",
+				link: "https://tauri.app/",
+			},
+			{
+				name: "ASP.NET",
+				link: "https://dotnet.microsoft.com/apps/aspnet",
+			},
+		],
+	},
+	{
+		category: "Database",
+		children: [
+			{
+				name: "MySQL",
+				link: "https://www.mysql.com/",
+			},
+			{
+				name: "SQLite",
+				link: "https://www.sqlite.org/index.html",
+			},
+		],
+	},
+	{
+		category: "Content Management System",
+		children: [
+			{
+				name: "Sanity",
+				link: "https://www.sanity.io/",
+			},
+		],
+	},
+];
