cd28ff260ed7a68e7c858375f088c6b7edcda62c
Rafli Malik <Rafli.Malik@gmail.com>
2024-10-11 23:14:38 +0700

    feat: add education and work

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,20 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
 
-import { skillsList, projectsList } from "@/lib/data";
+import {
+	skillsList,
+	projectsList,
+	educationList,
+	experienceList,
+	otherExperienceList,
+} from "@/lib/data";
+
+import { SquareArrowRight } from "lucide-react";
 
 export default function Home() {
 	return (
-		<main className="flex w-full flex-col items-stretch justify-start gap-8">
+		<main className="mb-48 flex w-full flex-col items-stretch justify-start gap-8">
 			<section id="intro" className="flex h-dvh w-full flex-col items-start justify-center gap-4">
 				<h1 className="text-4xl font-light leading-none tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
 					Rafli Malik
@@ -40,20 +47,37 @@ export default function Home() {
 						{skillsList?.map((skill) => (
 							<li key={skill.category}>
 								<h3 className="text-2xl font-semibold tracking-tight">{skill.category}</h3>
-								<Separator orientation="horizontal" className="mb-4" />
+								<hr className="mb-4 shrink-0 border border-neutral-200 delay-200 duration-500 ease-out dark:border-neutral-800" />
 								<ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 									{skill?.children.map((child) => (
 										<Link href={child.link} key={child.name} className="">
 											<Card className="w-full" style={{ color: `#${child.color ?? "000000"}` }}>
 												<CardHeader className="p-4">
 													<CardTitle className="flex items-center gap-2 text-xl font-normal">
-														{child.logo ? (
+														{child.logo && child.logoAlt ? (
+															<>
+																<Image
+																	src={child.logo ?? ""}
+																	width={20}
+																	height={20}
+																	alt=""
+																	className="block h-auto dark:hidden"
+																/>
+																<Image
+																	src={child.logoAlt ?? ""}
+																	width={20}
+																	height={20}
+																	alt=""
+																	className="hidden h-auto dark:block"
+																/>
+															</>
+														) : child.logo ? (
 															<Image
 																src={child.logo ?? ""}
 																width={20}
 																height={20}
-																alt="logo"
-																className=""
+																alt=""
+																className="h-auto"
 															/>
 														) : null}
 														{child.name}
@@ -76,12 +100,12 @@ export default function Home() {
 					maxime commodi sunt architecto quaerat, dignissimos cumque repudiandae nesciunt at.
 					Dolores itaque iusto harum, neque iste placeat reprehenderit?
 				</p>
-				
+
 				<ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
 					{projectsList?.map((project, index) => (
 						<li key={project.name} className="items-stretch">
 							<Card
-								className="group flex h-full flex-col justify-between duration-200 starting:opacity-0"
+								className="group flex h-full w-full flex-col justify-between duration-200 starting:opacity-0"
 								style={{ transitionDelay: `${index * 3}0ms` }}
 							>
 								<CardHeader>
@@ -102,6 +126,67 @@ export default function Home() {
 									</ul>
 								</CardFooter>
 							</Card>
+						</li>
+					))}
+				</ul>
+			</section>
+
+			<section id="experience" className="space-y-4">
+				<h2 className="text-4xl font-semibold tracking-tighter">Experience</h2>
+				<section>
+					<h3 className="text-xl font-medium tracking-tight">Work Experience</h3>
+					<hr className="mb-1 shrink-0 border border-neutral-200 delay-200 duration-500 ease-out dark:border-neutral-800" />
+					<ul className="flex flex-col gap-4">
+						{experienceList?.map((experience) => (
+							<li key={experience.company} className="flex flex-col items-start gap-0 duration-200">
+								<h4 className="group relative text-lg font-semibold">{experience.company}</h4>
+								<p className="text-neutral-500">{experience.position}</p>
+								<p className="text-neutral-500">
+									{experience.from} &ndash; {experience.to}
+								</p>
+							</li>
+						))}
+					</ul>
+				</section>
+				<section>
+					<h3 className="text-xl font-medium tracking-tight">Other Experience</h3>
+					<hr className="mb-1 shrink-0 border border-neutral-200 delay-200 duration-500 ease-out dark:border-neutral-800" />
+					<ul className="flex flex-col gap-4">
+						{otherExperienceList?.map((experience) => (
+							<li key={experience.company} className="flex flex-col items-start gap-0 duration-200">
+								<Link href={experience.externalLink ?? ""} className="group duration-200">
+									<h4 className="flex items-center gap-0 overflow-hidden text-lg font-semibold duration-200 group-hover:text-pink-500 group-focus:text-pink-500">
+										{experience.company}
+										<span className="-ml-px overflow-hidden">
+											<SquareArrowRight className="ml-2 h-full w-auto -translate-x-full text-pink-500 opacity-0 duration-300 ease-[cubic-bezier(0.83,0,0.17,1)] group-hover:translate-x-0 group-hover:opacity-100 group-focus:translate-x-0 group-focus:opacity-100" />
+										</span>
+									</h4>
+									<p className="text-neutral-500">{experience.position}</p>
+									<p className="text-neutral-500">
+										{experience.from} &ndash; {experience.to}
+									</p>
+								</Link>
+							</li>
+						))}
+					</ul>
+				</section>
+			</section>
+
+			<section>
+				<h2 className="text-4xl font-semibold tracking-tighter">Education</h2>
+				<ul className="ml-2 flex flex-col gap-4 border-l border-pink-500">
+					{educationList?.map((education) => (
+						<li
+							key={education.school}
+							className="relative flex flex-col items-start gap-0 pl-4 duration-200"
+						>
+							<div className="absolute -left-1.5 mt-2 h-3 w-3 bg-pink-500"></div>
+							<h4 className="text-lg font-semibold">{education.school}</h4>
+							<p>{education.major}</p>
+							<p>
+								{education.from} &ndash; {education.to}
+							</p>
+							<p>{education.grade}</p>
 						</li>
 					))}
 				</ul>
