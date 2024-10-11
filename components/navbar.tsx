a3c41a7e01ac7afcd60e71355473329e85e46c69
Rafli Malik <Rafli.Malik@gmail.com>
2024-10-11 22:10:52 +0700

    style: change hover animation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -25,15 +25,13 @@ const navLink: NavLink[] = [
 export function Navbar() {
 	return (
 		<nav className="fixed top-0 left-0 z-10 flex h-fit w-full flex-row items-center gap-8 bg-white/80 py-2 px-6 px-8 backdrop-blur-md backdrop-saturate-150 sm:px-10 md:px-14 lg:px-20 xl:px-24">
-			<ul className="flex flex-row items-center justify-start gap-16 font-medium lg:text-xl">
+			<ul className="flex flex-row items-center justify-start gap-12 font-medium lg:gap-14 lg:text-xl xl:gap-16">
 				{navLink.map((link) => {
 					return (
 						<li key={link.href} className="group">
-							<Link
-								href={link.href}
-								className="duration-200 ease-out group-hover:underline hover:text-pink-500"
-							>
+							<Link href={link.href} className="group relative py-1 px-3 duration-200 ease-out">
 								{link.title}
+								<div className="absolute top-0 left-0 z-[-1] h-full w-full origin-right scale-x-0 bg-pink-100 transition-transform duration-300 ease-[cubic-bezier(0.83,0,0.17,1)] group-hover:left-0 group-hover:w-full group-hover:origin-left group-hover:scale-x-100 group-focus:w-full motion-reduce:duration-0"></div>
 							</Link>
 						</li>
 					);
