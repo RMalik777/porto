15c7b592793eb9df47a617b81b692029ee0bbf11
Rafli Malik <Rafli.Malik@gmail.com>
2024-10-20 15:12:18 +0700

    chore: swap navbar position

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,16 +10,16 @@ const navLink: NavLink[] = [
 		href: "/#intro",
 	},
 	{
-		title: "About",
-		href: "/#about",
-	},
-	{
 		title: "Skills",
 		href: "/#skills",
 	},
 	{
 		title: "Projects",
 		href: "/#projects",
+	},
+	{
+		title: "About",
+		href: "/#about",
 	},
 ];
 export function Navbar() {
@@ -31,10 +31,10 @@ export function Navbar() {
 						<li key={link.href} className="group">
 							<Link
 								href={link.href}
-								className="group relative py-1 px-3 delay-300 duration-150 ease-out hover:text-white focus:text-white"
+								className="group relative py-1 px-3 delay-300 duration-150 ease-out hover:text-white focus-visible::text-white"
 							>
 								{link.title}
-								<div className="absolute top-0 left-0 z-[-1] h-full w-full origin-right scale-x-0 bg-fuchsia-500 transition-transform duration-300 ease-[cubic-bezier(0.83,0,0.17,1)] group-hover:origin-left group-hover:scale-x-100 group-focus:origin-left group-focus:scale-x-100 motion-reduce:duration-0"></div>
+								<div className="absolute top-0 left-0 z-[-1] h-full w-full origin-right scale-x-0 bg-fuchsia-500 transition-transform duration-300 ease-[cubic-bezier(0.83,0,0.17,1)] group-hover:origin-left group-hover:scale-x-100 group-focus-visible:origin-left group-focus-visible:scale-x-100 motion-reduce:duration-0"></div>
 							</Link>
 						</li>
 					);
