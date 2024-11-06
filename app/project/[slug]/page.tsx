35ae89bd02281e938a8172cdabe2d13c3ec5d5a2
Rafli Malik <Rafli.Malik@gmail.com>
2024-11-06 14:51:50 +0700

    feat: add page for project

---
+++ app/project/[slug]/page.tsx
@@ -0,0 +1,110 @@
+import Image from "next/image";
+import Link from "next/link";
+import type { Metadata } from "next";
+
+import { projectsList } from "@/lib/data/project";
+
+import { Button } from "@/components/ui/button";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export const dynamicParams = false;
+
+export async function generateStaticParams() {
+	const posts = projectsList;
+	return posts.map((post) => {
+		return {
+			params: {
+				slug: post.name.replace(/\s+/g, "-").toLowerCase(),
+			},
+		};
+	});
+}
+
+export const metadata: Metadata = {
+	title: "",
+	description: "",
+};
+
+export default async function Page({ params }: Readonly<{ params: Promise<{ slug: string }> }>) {
+	const slug = (await params).slug;
+	const post = projectsList.find((post) => post.name.replace(/\s+/g, "-").toLowerCase() === slug);
+
+	metadata.title = post?.name;
+	metadata.description = post?.desc;
+
+	return (
+		<main className="mt-20 mb-20 flex items-center justify-center px-6 text-black sm:mb-24 md:mt-36 md:mb-32 md:px-10 lg:mb-36 xl:mb-48 dark:text-white">
+			<section className="flex w-full max-w-prose flex-col items-center justify-start gap-1 tracking-tight">
+				{post ? (
+					<>
+						<h1 className="mt-4 text-center text-3xl font-semibold tracking-tighter sm:text-4xl">
+							{post.name}
+						</h1>
+						<p className="text-center text-lg">{post?.desc}</p>
+						<ul className="mt-1 flex flex-row flex-wrap items-center justify-center gap-2">
+							{post.tech.map((tech) => (
+								<li
+									className="bg-violet-100 px-2 py-1 text-xs font-medium text-violet-700"
+									key={tech}
+								>
+									{tech}
+								</li>
+							))}
+						</ul>
+						<hr className="mb-px h-px w-full duration-500 starting:w-0" />
+						<div className="my-2 flex w-full flex-row items-center justify-evenly gap-2">
+							<Button
+								variant="outline"
+								className="w-full"
+								disabled={!post.live}
+								asChild={!!post.live}
+							>
+								{post.live ? <Link href={post.live}>Live</Link> : "Preview"}
+							</Button>
+							<Button variant="outline" className="w-full" asChild>
+								<Link href={post.source}>Source</Link>
+							</Button>
+						</div>
+						<p className="text-left">{post?.longdesc}</p>
+						<figure>
+							<Carousel
+								opts={{
+									align: "center",
+									loop: true,
+									duration: 50,
+								}}
+							>
+								<CarouselContent>
+									{post.image.map((image) => (
+										<CarouselItem key={image}>
+											<Image
+												src={image}
+												alt={post.name}
+												width={1920}
+												height={1080}
+												className="h-auto w-full"
+											/>
+										</CarouselItem>
+									))}
+								</CarouselContent>
+								<CarouselPrevious />
+								<CarouselNext />
+							</Carousel>
+							<figcaption className="text-sm text-neutral-500">Preview</figcaption>
+						</figure>
+					</>
+				) : (
+					<>
+						<h1>404 - Not Found</h1>
+					</>
+				)}
+			</section>
+		</main>
+	);
+}
--- lib/data/project.ts
+++ lib/data/project.ts
@@ -1,8 +1,8 @@
 interface Project {
 	name: string;
 	desc: string;
-  longdesc?: string;
-	image: string;
+	longdesc?: string;
+	image: string[];
 	live?: string;
 	source: string;
 	tech: string[];
@@ -12,7 +12,7 @@ export const projectsList: Project[] = [
 	{
 		name: "Blog Website",
 		desc: "A blog website to write and share my thoughts and experiences.",
-		image: "/Project/thumbnail/blog.webp",
+		image: ["/Project/thumbnail/blog.webp"],
 		live: "https://blog.raflimalik.com",
 		source: "https://github.com/RMalik777/blog",
 		tech: ["Astro", "Sanity", "Tailwind CSS"],
@@ -20,7 +20,7 @@ export const projectsList: Project[] = [
 	{
 		name: "Link Book",
 		desc: "Self created linktree alternative to host my social media links.",
-		image: "/Project/thumbnail/linkbook.webp",
+		image: ["/Project/thumbnail/linkbook.webp"],
 		live: "https://link.raflimalik.com",
 		source: "https://github.com/RMalik777/LinkBook",
 		tech: ["SvelteKit", "Tailwind CSS"],
@@ -28,7 +28,7 @@ export const projectsList: Project[] = [
 	{
 		name: "Tech Event Website",
 		desc: "Website for a imaginary tech event called “Pudding Fest” by PT Musang.",
-		image: "/Project/thumbnail/puddingfest.webp",
+		image: ["/Project/thumbnail/puddingfest.webp"],
 		live: "https://puddingfest.raflimalik.com",
 		source: "https://github.com/RMalik777/BNCC-Final-Project",
 		tech: ["HTML", "CSS", "Bootstrap", "JavaScript", "jQuery", "Firebase Firestore"],
@@ -36,7 +36,7 @@ export const projectsList: Project[] = [
 	{
 		name: "Mini Portfolio",
 		desc: "Website to show personal information.",
-		image: "/Project/thumbnail/miniporto.webp",
+		image: ["/Project/thumbnail/miniporto.webp"],
 		live: "https://miniporto.raflimalik.com",
 		source: "https://github.com/RMalik777/BNCC-Mid-Project",
 		tech: ["HTML", "CSS", "JavaScript"],
@@ -44,7 +44,7 @@ export const projectsList: Project[] = [
 	{
 		name: "KpopZtation",
 		desc: "Website for a music album shopping.",
-		image: "/Project/thumbnail/kpopztation.webp",
+		image: ["/Project/thumbnail/kpopztation.webp"],
 		live: "",
 		source: "https://github.com/RMalik777/PSD_Lab",
 		tech: ["ASP.NET", "SQL Server"],
@@ -52,7 +52,7 @@ export const projectsList: Project[] = [
 	{
 		name: "Astronom",
 		desc: "A space themed website. Containing information and images from Hubble Space Telescope.",
-		image: "/Project/thumbnail/astronom.webp",
+		image: ["/Project/thumbnail/astronom.webp"],
 		live: "https://astronom.raflimalik.com",
 		source: "https://github.com/RMalik777/Astronom",
 		tech: ["HTML", "CSS"],
@@ -60,7 +60,7 @@ export const projectsList: Project[] = [
 	{
 		name: "NOKA",
 		desc: "Website for a game launch event. Contain information about the launch date, future plan and esports.",
-		image: "/Project/thumbnail/NOKA.webp",
+		image: ["/Project/thumbnail/NOKA.webp"],
 		live: "https://noka.raflimalik.com",
 		source: "https://github.com/RMalik777/HCI_Project",
 		tech: ["HTML", "CSS"],
@@ -68,7 +68,7 @@ export const projectsList: Project[] = [
 	{
 		name: "Tourism App",
 		desc: "Mobile app to promote tourism in Komodo Island. Contains features like emergency notification, place recommendation, and guidelines.",
-		image: "/Project/thumbnail/Tourism.webp",
+		image: ["/Project/thumbnail/Tourism.webp"],
 		live: "https://www.figma.com/proto/yvdpsRxRriroCLBme1aMEq/HCI---Final-Exam-2?page-id=2%3A2&type=design&node-id=4-24&viewport=589%2C294%2C0.09&t=A3L94ZgtCx0jDe3n-1&scaling=scale-down&starting-point-node-id=4%3A24&mode=design",
 		source:
 			"https://www.figma.com/file/yvdpsRxRriroCLBme1aMEq/HCI---Final-Exam-2?type=design&node-id=2%3A2&mode=design&t=udRyIo2NUwXqWOdV-1",
@@ -77,7 +77,7 @@ export const projectsList: Project[] = [
 	{
 		name: "Wedding Planner",
 		desc: "Mobile application for wedding venue finder. The app can help users search, book, and pay for the place of their wedding.",
-		image: "/Project/thumbnail/Wedding Planner.webp",
+		image: ["/Project/thumbnail/Wedding Planner.webp"],
 		live: "https://www.figma.com/proto/tZ1g8qfiOXy4oYlyzwFPLr/HCI---Mid-Exam?page-id=0%3A1&type=design&node-id=2-40&viewport=294%2C568%2C0.47&t=2hrk8OTbLjy3ofxQ-1&scaling=scale-down&starting-point-node-id=2%3A40&mode=design",
 		source:
 			"https://www.figma.com/file/tZ1g8qfiOXy4oYlyzwFPLr/HCI---Mid-Exam?type=design&node-id=0%3A1&mode=design&t=hhIiCalCCuqud4Y0-1",
@@ -86,7 +86,7 @@ export const projectsList: Project[] = [
 	{
 		name: "E-Commerce",
 		desc: "E-Commerce mobile application. The app contains features like integrated payment, recommendation and comprehensive detail.",
-		image: "/Project/thumbnail/ECommerce.webp",
+		image: ["/Project/thumbnail/ECommerce.webp"],
 		live: "https://www.figma.com/proto/bij8OTQPuLDjDyLsDVeNHv/HCI---Project?page-id=0%3A1&type=design&node-id=7-2&viewport=405%2C460%2C0.1&t=CcW2mXRwhCLusWs4-1&scaling=scale-down&starting-point-node-id=7%3A2&mode=design",
 		source:
 			"https://www.figma.com/file/bij8OTQPuLDjDyLsDVeNHv/HCI---Project?type=design&node-id=0%3A1&mode=design&t=mosRZ5ZIb083OhTC-1",
