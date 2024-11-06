67e9a59185450ea664a91fdeb7a4d31cd56daf29
Rafli Malik <Rafli.Malik@gmail.com>
2024-11-06 15:03:09 +0700

    chore: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,26 @@
 import { MetadataRoute } from "next";
+import { projectsList } from "@/lib/data/project";
 
+const ROOT_URL = "https://raflimalik.com";
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: "https://raflimalik.com",
+			url: ROOT_URL,
 			lastModified: new Date(),
 			changeFrequency: "yearly",
 			priority: 1,
 		},
 		{
-			url: "https://raflimalik.com/license",
+			url: `${ROOT_URL}/about`,
 			lastModified: new Date(),
 			changeFrequency: "yearly",
-			priority: 0.5,
+			priority: 0.2,
 		},
+		...projectsList.map((project) => ({
+			url: `${ROOT_URL}/${project.name.replace(/\s+/g, "-").toLowerCase()}`,
+			lastModified: new Date(),
+			changeFrequency: "yearly" as const,
+			priority: 0.5,
+		})),
 	];
 }
